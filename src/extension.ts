37d41c9ad83f7af431296a374a40bb122fd65be1
SnaveSutit <snavesutit@gmail.com>
2024-05-05 17:33:03 -0400

    ⬆️ Update to MC-Build 3.1.1
    - Added `enchantment` json file type.
    - Fixed execute run blocks not always highlighting correctly.
    - Add dist folder to `.gitignore`

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,41 @@
 import * as vscode from 'vscode'
-import * as mcbuild from 'mc-build'
 import * as fs from 'fs'
 import * as pathjs from 'path'
 import { VariableMap } from 'mc-build/dist/mcl/Compiler'
+import * as childProcess from 'child_process'
 // import * as crypto from 'crypto'
+
+function runMCB(args: string[]) {
+	const process = childProcess.spawn('mcb', args, {
+		stdio: 'pipe',
+		shell: true,
+	})
+}
 
 export async function activate(context: vscode.ExtensionContext) {
 	console.log('MCBuild Activated!')
+
+	// const process = childProcess
+	// 	.spawn('npm', ['i', '-g', 'mc-build@latest'], {
+	// 		stdio: 'pipe',
+	// 		shell: true,
+	// 	})
+	// 	.on('exit', code => {
+	// 		if (code !== 0) {
+	// 			vscode.window.showErrorMessage('Failed to install mc-build!')
+	// 		}
+	// 	})
+	// 	.on('error', err => {
+	// 		vscode.window.showErrorMessage('Failed to install mc-build!')
+	// 	})
+
+	// process.stdout!.on('data', data => {
+	// 	console.log(data.toString())
+	// })
+
+	// process.stderr!.on('data', data => {
+	// 	console.log(data.toString())
+	// })
 
 	vscode.commands.registerCommand('mc-build.build', async () => {
 		if (!vscode.workspace.workspaceFolders) {
@@ -16,94 +45,8 @@ export async function activate(context: vscode.ExtensionContext) {
 
 		const workspaceFolder = vscode.workspace.workspaceFolders[0]
 		const rootFolder = workspaceFolder.uri.fsPath
-		const configJsFile = pathjs.join(rootFolder, 'mcb.config.js')
-		const configJSONFile = pathjs.join(rootFolder, 'mcb.config.json')
-		const srcFolder = pathjs.join(rootFolder, 'src')
-		// const mcbFolder = pathjs.join(rootFolder, '.mcb')
-		// const mcbFsCache = pathjs.join(mcbFolder, 'fs-cache.txt')
 
-		if (!fs.existsSync(srcFolder)) {
-			vscode.window.showErrorMessage(`No src folder found!`)
-			return
-		}
-
-		let config: any = {}
-		try {
-			if (fs.existsSync(configJsFile)) {
-				config = eval(await fs.promises.readFile(configJsFile, 'utf8'))
-			} else if (fs.existsSync(configJSONFile)) {
-				config = JSON.parse(await fs.promises.readFile(configJSONFile, 'utf8'))
-			} else {
-				vscode.window.showErrorMessage(`No config file found!`)
-				return
-			}
-		} catch (e) {
-			vscode.window.showErrorMessage(`Error parsing MCBuild config file: ${e}`)
-			return
-		}
-		console.log('Config:', config)
-
-		console.log('Building...')
-		const compiler = new mcbuild.Compiler(srcFolder, config)
-
-		const fileCache = new Map<string, string>()
-		const customIO = new mcbuild.SyncIo()
-		customIO.write = (localPath, content) => {
-			const writePath = pathjs.join(rootFolder, localPath)
-			console.log('Write:', writePath)
-			fileCache.set(writePath, content)
-		}
-		compiler.io = customIO
-
-		async function recurseSrc(path: string) {
-			console.log('Recurse:', path)
-			if (!fs.existsSync(path)) return
-			const stat = await fs.promises.stat(path)
-			if (stat.isDirectory()) {
-				const files = await fs.promises.readdir(path)
-				for (const file of files) {
-					await recurseSrc(pathjs.join(path, file))
-				}
-			} else {
-				const content = await fs.promises.readFile(path, 'utf8')
-				const tokens = mcbuild.Tokenizer.tokenize(content, path)
-				switch (pathjs.extname(path)) {
-					case '.mcb': {
-						const ast = mcbuild.Parser.parseMcbFile(tokens)
-						compiler.addFile(path, ast)
-						break
-					}
-					case '.mcbt': {
-						const ast = mcbuild.Parser.parseMcbtFile(tokens)
-						compiler.addFile(path, ast)
-						break
-					}
-				}
-			}
-		}
-
-		await recurseSrc(srcFolder)
-		console.log('Compiling...')
-		try {
-			compiler.compile(VariableMap.fromObject({}))
-		} catch (e) {
-			vscode.window.showErrorMessage(`Encountered error compiling MCBuild Project: ${e}`)
-			return
-		}
-		console.log('Compiled!')
-
-		console.log('File cache:', fileCache)
-		console.log('Writing files...')
-
-		const folderCache = new Set<string>()
-		for (const [path, content] of fileCache) {
-			const dirName = pathjs.dirname(path)
-			if (!folderCache.has(dirName)) {
-				await fs.promises.mkdir(dirName, { recursive: true })
-				folderCache.add(dirName)
-			}
-			await fs.promises.writeFile(path, content, 'utf8')
-		}
+		// childProcess.ex
 
 		console.log('Build complete!')
 		vscode.window.showInformationMessage('Build complete!')
